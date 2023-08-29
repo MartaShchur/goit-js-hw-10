b5de3d8e3f7cd242c71d33f3958f60b9783c1fdd
MartaShchur <m.matseyko199@gmail.com>
2023-08-29 17:35:04 +0200

    add commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import './styles.css';
 // import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
-
 import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
   'live_c2vp58LgyS5g8ljIA3yluFIy4vOl1HNEOPZ9uLLOcMAoHfvcGzkdSmRdR0PTOewC';
@@ -14,11 +13,9 @@ const ref = {
   loader: document.querySelector('.loader'),
   p_error: document.querySelector('.error'),
 };
-const { selector, divCatInfo, loader,  p_error } = ref;
+const { selector, divCatInfo, loader, p_error } = ref;
 
 divCatInfo.classList.add('is-hidden');
-
-
 
 let arrBreedsId = [];
 
@@ -35,12 +32,26 @@ fetchBreeds()
       select: selector,
       data: arrBreedsId,
     });
+      selector.addEventListener('change', onSelectBreed);
   })
-  .catch(err => {});
+  .catch(err => { });
+  
+function displayCatInfo(catData) {
+    
+    const { url, breeds } = catData[0];
 
-selector.addEventListener('change', onSelectBreed);
+  divCatInfo.innerHTML = `<div class="box-img">
+    <img src="${url}" alt="${breeds[0].name}" width="400"/></div>
+    <div class="box"><h1>${breeds[0].name}</h1>
+    <p>${breeds[0].description}</p>
+    <p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`;
+
+  loader.classList.replace('loader', 'is-hidden');
+  divCatInfo.classList.remove('is-hidden');
+}
 
 function onSelectBreed(evt) {
+
   divCatInfo.classList.add('is-hidden');
     loader.classList.replace('is-hidden', 'loader');
     p_error.classList.add('is-hidden');
@@ -50,21 +61,25 @@ function onSelectBreed(evt) {
   fetchCatByBreed(breedId)
       .then(data => {
         // console.log(data)
-          if (data.length === 0) {
+        if (data.length === 0) {
+            
         loader.classList.replace('loader', 'is-hidden');   
         p_error.classList.remove('is-hidden');
 
               return     
         }
-      const { url, breeds } = data[0];
-      divCatInfo.innerHTML = `<div class="box-img">
-        <img src="${url}" alt="${breeds[0].name}" width="400"/></div>
-        <div class="box"><h1>${breeds[0].name}</h1>
-        <p>${breeds[0].description}</p>
-        <p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`;
 
-    loader.classList.replace('loader', 'is-hidden');
-    divCatInfo.classList.remove('is-hidden');
+        displayCatInfo(data);
+    //   const { url, breeds } = data[0];
+    //   divCatInfo.innerHTML = `<div class="box-img">
+    //     <img src="${url}" alt="${breeds[0].name}" width="400"/></div>
+    //     <div class="box"><h1>${breeds[0].name}</h1>
+    //     <p>${breeds[0].description}</p>
+    //     <p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`;
+
+    // loader.classList.replace('loader', 'is-hidden');
+    // divCatInfo.classList.remove('is-hidden');
+        
     })
       .catch ( (err)=> {
           console.log(err)
